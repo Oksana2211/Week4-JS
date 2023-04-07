4d25e0993393c772e6f7d309506cde116a5dec83
Тайсумова Оксана <119331948+Oksana2211@users.noreply.github.com>
2023-04-07 15:06:19 +0300

    add function

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ let userComment = document.querySelectorAll('textarea')[0];
 let photo = document.querySelector('.user-photo');
 let userChat = document.querySelectorAll('textarea')[1];
 
-
-
 function sending(){ 
 let userNameV = userName.value;
 let arr = userNameV.split(' ');
@@ -21,10 +19,35 @@ userNameV = (userNameV1[0].toUpperCase() + (userNameV1.slice(1)).toLowerCase())+
 let userPhotoV = userPhoto.value;
 photo.innerHTML = `<img class="user-photo" src=${userPhotoV} alt="фото пользователя">`;
 
-let userCommentV = userComment.value;
+let userCommentV = userComment.value.toLowerCase();
 
+
+if(userCommentV.includes('viagra')){
+userCommentV = userCommentV.replace(/viagra/gi, "***");
 userChat.value =`
 ${userNameV}
-${userCommentV}`;
+${userCommentV}`}
+if (userCommentV.includes('xxx')){
+userCommentV = userCommentV.replace(/xxx/gi, "***");
+userChat.value =`
+${userNameV}
+${userCommentV}`
 }
+else {
+userChat.value =`
+${userNameV}
+${userCommentV}`
+}
+}
+
+
+/// задание со звездочкой
+let text1 = document.querySelectorAll('textarea')[2];
+let text2 = document.querySelectorAll('textarea')[3];
+
+function deleteTags() {
+  let textValue = text1.value;
+  textValue = textValue.replace(/<\/?[a-zA-Z]+>/gi,'');
+  text2.value = textValue;
+};
 
