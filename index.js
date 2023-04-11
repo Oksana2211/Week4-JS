e2d74c25a1eaa26c0cc61366e072a2aec263a534
Тайсумова Оксана <119331948+Oksana2211@users.noreply.github.com>
2023-04-11 13:35:58 +0300

    correction

--- index.js
+++ index.js
@@ -12,9 +12,17 @@ let userNameV1 = arr[0];
 let userNameV2 = arr[1];
 let userNameV3 = arr[2];
 
-userNameV = (userNameV1[0].toUpperCase() + (userNameV1.slice(1)).toLowerCase())+`${' '}`
+if(arr.length >=3) {
+userNameV =
+(userNameV1[0].toUpperCase() + (userNameV1.slice(1)).toLowerCase())+`${' '}`
 +(userNameV2[0].toUpperCase() + (userNameV2.slice(1)).toLowerCase())+`${' '}`
 +(userNameV3[0].toUpperCase() + (userNameV3.slice(1)).toLowerCase());
+} else if (arr.length ===2){
+  userNameV =
+(userNameV1[0].toUpperCase() + (userNameV1.slice(1)).toLowerCase())+`${' '}`
++(userNameV2[0].toUpperCase() + (userNameV2.slice(1)).toLowerCase());
+} else {userNameV =
+userNameV1[0].toUpperCase() + (userNameV1.slice(1)).toLowerCase();}
 
 let userPhotoV = userPhoto.value;
 photo.innerHTML = `<img class="user-photo" src=${userPhotoV} alt="фото пользователя">`;
